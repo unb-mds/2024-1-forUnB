c57418c830327aec5cf955df8322eb179e3d4088
Davi Levy <daviklevy@gmail.com>
2024-07-06 12:21:13 -0300

    Creating AnswerModelTest

--- backend/backendforum/main/tests/test_models.py
+++ backend/backendforum/main/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from main.models import Forum
+from main.models import *
 from django.utils import timezone
 
 class ForumModelTest(TestCase):
@@ -28,4 +28,33 @@ class ForumModelTest(TestCase):
         self.assertTrue(now - self.forum.created_at < timezone.timedelta(seconds=1))
 
 
+class AnswerModelTest(TestCase):
+
+    def setUp(self):
+        self.forum = Forum.objects.create(
+            title="Test Forum",
+            description="This is a test forum description"
+        )
+        self.answer = Answer.objects.create(
+            forum=self.forum,
+            text="This is a test answer text that is longer than fifty characters to test the __str__ method."
+        )
+
+    #testa se o objeto Answer é criado corretamente, se seus atributos são definidos conforme esperado e se está associado corretamente ao objeto Forum.   
+    def test_answer_creation(self):
+        self.assertIsInstance(self.answer, Answer)
+        self.assertEqual(self.answer.forum, self.forum)
+        self.assertEqual(self.answer.text, "This is a test answer text that is longer than fifty characters to test the __str__ method.")
+        self.assertIsNotNone(self.answer.created_at)
+
+    
+    # testa o método __str__ do modelo para garantir que ele retorne os primeiros 50 caracteres do texto da resposta, seguidos por '...'.
+    def test_str_method(self):
+        self.assertEqual(str(self.answer), "This is a test answer text that is longer than fif...")
+
+    
+    # testa se o campo created_at é automaticamente preenchido com a data e hora atuais quando um objeto Answer é criado.
+    def test_created_at_auto_now_add(self):
+        now = timezone.now()
+        self.assertTrue(now - self.answer.created_at < timezone.timedelta(seconds=1)) 
     
