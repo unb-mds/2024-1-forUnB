fb120e356626db6f9d68f914ac6bf7b892ad245d
Davi Levy <daviklevy@gmail.com>
2024-07-06 12:34:41 -0300

    Creating IndexViewTest

--- backend/backendforum/main/tests/test_views.py
+++ backend/backendforum/main/tests/test_views.py
@@ -1,4 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class IndexViewTest(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+
+    # testa se a view index retorna um código de status HTTP 200, indicando que a página foi carregada com sucesso.
+    def test_index_view_status_code(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    
+    # testa se a view index usa o template correto, 'main/index.html'.
+    def test_index_view_template_used(self):
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'main/index.html')
