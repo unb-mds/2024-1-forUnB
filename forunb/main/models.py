36d57195dca2d9e2baf5ed3f331f137094e32f3b
Davi Levy <daviklevy@gmail.com>
2024-07-19 11:00:46 -0300

    Feat: Updating Forum model

--- forunb/main/models.py
+++ forunb/main/models.py
@@ -1,18 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Forum(models.Model):
-    """Criação do modelo de Fórum."""
     title = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        """Visualização do title pelo banco de dados."""
         return self.title
+
+    def get_questions(self):
+        return self.questions.all()  # Retorna todas as perguntas associadas a este fórum
 
 
 class Answer(models.Model):
