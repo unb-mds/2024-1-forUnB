e8a689488eb4a23718dc885f2a48fc02d6bfbab4
Davi-KLevy <daviklevy@gmail.com>
2024-07-30 16:18:10 -0300

    Fixing little details about the new Customuser

--- forunb/users/models.py
+++ forunb/users/models.py
@@ -2,24 +2,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
+        user = self.model(email=email, username=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(email, username, password, **extra_fields)
+        return self.create_user(email, password=password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=100, unique=True, blank=True, null=True)
     #photo = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     followed_forums = models.ManyToManyField('main.Forum', blank=True, related_name='followers')
     liked_questions = models.ManyToManyField('main.Question', blank=True, related_name='liked_by')
@@ -32,7 +31,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.username
--- forunb/users/views.py
+++ forunb/users/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .forms import UnbEmailRegistrationForm
+
 
 # def register(request):
 #     if request.method != 'POST':
@@ -23,11 +23,11 @@ from .forms import UnbEmailRegistrationForm
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .forms import UnbEmailRegistrationForm
+from .forms import CustomUserCreationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UnbEmailRegistrationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
@@ -36,7 +36,7 @@ def register(request):
             login(request, aunthenticated_user)
             return redirect('index')
     else:
-        form = UnbEmailRegistrationForm()
+        form = CustomUserCreationForm()
     
     return render(request, 'users/register_unb_email.html', {'form': form})
 
