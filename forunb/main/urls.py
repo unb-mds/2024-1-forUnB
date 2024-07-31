14be94a2e40d8e010c714dd894b88447cc652a37
BrunoBReis <brunobragancadosreis@gmail.com>
2024-07-31 13:35:41 -0300

    bug: removing broken url

--- forunb/main/urls.py
+++ forunb/main/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('forums/', views.forum_list, name='forum_list'),
     path('new_question/<int:forum_id>/', views.new_question, name='new_question'),
     path('new_answer/<int:question_id>/', views.new_answer, name='new_answer'),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # Estes paths entraram em desuso, vou utilizar os caminhos do app users
+    # path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    # path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
