b2d43e4132e5d1225b6899d14b330ac7737cbb69
Davi-KLevy <daviklevy@gmail.com>
2024-07-31 17:11:30 -0300

    Feat: Creating search view

--- forunb/search/views.py
+++ forunb/search/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from main.models import Forum
 
-# Create your views here.
+def search_forum(request):
+    query = request.GET.get('search', '')  # Obtém o parâmetro de pesquisa da URL
+    if query:
+        try:
+            forum = Forum.objects.get(title__icontains=query)  # Busca o fórum que contém o texto da pesquisa
+            return redirect(reverse('forum_detail', args=[forum.id]))  # Redireciona para a página do fórum
+        except Forum.DoesNotExist:
+            return render(request, 'search/not_found.html', {'query': query})  # Página de não encontrado
+    return redirect('forum_list')
