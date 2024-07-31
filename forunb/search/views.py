ccb73bfd042b25fbdc3d9870f7a6bdd3942fca87
Davi-KLevy <daviklevy@gmail.com>
2024-07-31 17:16:16 -0300

    Update: Updating search view

--- forunb/search/views.py
+++ forunb/search/views.py
@@ -1,13 +1,7 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.shortcuts import render
 from main.models import Forum
 
 def search_forum(request):
     query = request.GET.get('search', '')  # Obtém o parâmetro de pesquisa da URL
-    if query:
-        try:
-            forum = Forum.objects.get(title__icontains=query)  # Busca o fórum que contém o texto da pesquisa
-            return redirect(reverse('forum_detail', args=[forum.id]))  # Redireciona para a página do fórum
-        except Forum.DoesNotExist:
-            return render(request, 'search/not_found.html', {'query': query})  # Página de não encontrado
-    return redirect('forum_list')
+    forums = Forum.objects.filter(title__icontains=query)  # Filtra os fóruns com base na pesquisa
+    return render(request, 'main/forums.html', {'forums': forums, 'query': query})  # Renderiza a página de fóruns com os resultados
