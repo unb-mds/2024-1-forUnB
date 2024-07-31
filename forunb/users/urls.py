38d217e2a1740881328312a66cfcae041276994c
BrunoBReis <brunobragancadosreis@gmail.com>
2024-07-31 13:36:01 -0300

    bug: adding new url to users

--- forunb/users/urls.py
+++ forunb/users/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('logout/', views.Logout_view, name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
 # urlpatterns = [
 #     path('register/', views.register_unb_email, name='register_unb_email'),
