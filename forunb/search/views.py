28a947c6fb4032faba1309a7d039fb69823444c2
Davi-KLevy <daviklevy@gmail.com>
2024-08-01 16:36:28 -0300

    Feat: Updating search_forum

--- forunb/search/views.py
+++ forunb/search/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from main.models import Forum
+from django.shortcuts import render, get_object_or_404
+from main.models import Question, Forum
 
 def search_forum(request):
-    query = request.GET.get('search', '')  # Obtém o parâmetro de pesquisa da URL
-    forums = Forum.objects.filter(title__icontains=query)  # Filtra os fóruns com base na pesquisa
-    return render(request, 'main/forums.html', {'forums': forums, 'query': query})  # Renderiza a página de fóruns com os resultados
+    query = request.GET.get('search', '')
+    forum_id = request.GET.get('forum_id', None)  # Pega 'forum_id' ou None se não for passado
+
+    if forum_id:
+        forum = get_object_or_404(Forum, id=forum_id)
+        questions = Question.objects.filter(forum=forum, title__icontains=query).order_by('-created_at')
+        return render(request, 'main/forum_detail.html', {'forum': forum, 'questions': questions, 'query': query})
+    else:
+        forums = Forum.objects.filter(title__icontains=query)
+        return render(request, 'main/forums.html', {'forums': forums, 'query': query})
