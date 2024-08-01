87f7ec4d59c27216220da308a4fe316edb98834f
Davi-KLevy <daviklevy@gmail.com>
2024-08-01 16:17:51 -0300

    Bug: Fixing login redirect

--- forunb/main/views.py
+++ forunb/main/views.py
@@ -43,7 +43,7 @@ def question_detail(request, question_id):
     return render(request, 'main/question_detail.html', {'question': question, 'answers': answers})
 
 
-@login_required
+@login_required(login_url='/users/login')
 def new_question(request, forum_id):
     forum = get_object_or_404(Forum, id=forum_id)
     if request.method == 'POST':
@@ -59,7 +59,7 @@ def new_question(request, forum_id):
     return render(request, 'main/new_question.html', {'form': form, 'forum': forum})
 
 
-@login_required
+@login_required(login_url='/users/login')
 def new_answer(request, question_id):
     question = get_object_or_404(Question, id=question_id)
     if request.method == 'POST':
