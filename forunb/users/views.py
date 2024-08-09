c737a81e16e848f5f8a3f12db119625fb0959654
Victor Bernardes <vmousee10@gmail.com>
2024-08-09 17:30:04 -0300

    fix: deleting duplicate imports

--- forunb/users/views.py
+++ forunb/users/views.py
@@ -2,20 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import logout, login, authenticate
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-
-
-# views.py
-
-from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .forms import *
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-
 
 
 def register(request): 
