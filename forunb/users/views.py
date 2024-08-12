c528eccd6203751136e3654c82d31c4614f31490
Victor Bernardes <vmousee10@gmail.com>
2024-08-12 16:26:03 -0300

    fix: fixing password message error in views.py

--- forunb/users/views.py
+++ forunb/users/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
+
 # views.py
 
 from django.shortcuts import render, redirect
@@ -43,6 +44,20 @@ def Logout_view(request):
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
+        
+        '''Verifica se o email existe no banco de dados'''
+        email = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        if not CustomUser.objects.filter(email=email).exists():
+            error_message = "Este email não está cadastrado."
+            form.add_error('username', error_message)
+        
+        # Se o email existir, mas a senha estiver incorreta
+        elif form.is_valid() == False:
+            error_message = "Senha incorreta."
+            form.add_error('password', error_message)
+        
         if form.is_valid():
             login(request, form.get_user())
             if "next" in request.POST:
