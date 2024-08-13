b621e6f6ce00c9d61e1b2946594e074a3de546e2
BrunoBReis <brunobragancadosreis@gmail.com>
2024-08-13 09:32:36 -0300

    refactor: updating asgi with environ

--- forunb/forunb/asgi.py
+++ forunb/forunb/asgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
+from forunb.env import env, BASE_DIR
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forunb.settings')
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', env('DJANGO_SETTINGS_MODULE'))
 
 application = get_asgi_application()
