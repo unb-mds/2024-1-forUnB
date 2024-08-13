525e85915c5d86b890a82cd528328271ab2baa7b
BrunoBReis <brunobragancadosreis@gmail.com>
2024-08-13 19:06:37 -0300

    test: testing register view post valid

--- forunb/users/tests/test_views.py
+++ forunb/users/tests/test_views.py
@@ -17,3 +17,15 @@ class UserViewsTestCase(TestCase):
         response = self.client.get(reverse('users:register'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/register_unb_email.html')
+
+    def test_register_view_post_valid(self):
+        form_data = {
+            'email': 'newuser@aluno.unb.br',
+            'password1': 'newuserpassword123',
+            'password2': 'newuserpassword123'
+        }
+        response = self.client.post(reverse('users:register'), form_data)
+        # Should redirect to main:index
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(CustomUser.objects.filter(
+            email='newuser@aluno.unb.br').exists())
