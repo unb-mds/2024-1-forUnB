cf0cccd0ec3fd6d27b526d88507f3a71dc089239
Davi-KLevy <daviklevy@gmail.com>
2024-08-14 16:29:20 -0300

    Pylint: Fixing apps.py

--- forunb/users/apps.py
+++ forunb/users/apps.py
@@ -1,6 +1,9 @@
+"""App configuration for the users application."""
+
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """Configuration class for the users app."""    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
