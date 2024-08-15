96c190fb2b97df592b0af758fdb65e62d6b2c492
Davi-KLevy <daviklevy@gmail.com>
2024-08-15 10:33:03 -0300

    Pylint: Refactoring models.py

--- forunb/main/models.py
+++ forunb/main/models.py
@@ -1,146 +1,160 @@
-""" Models for the main app. """
+"""Models for the main app."""
+
 from django.db import models
 from django.conf import settings
-from users.models import CustomUser
 from PIL import Image
-
-
-# Create your models here.
+from users.models import CustomUser
 
 
 class Forum(models.Model):
-    """ Model for a forum. """
+    """Model for a forum."""
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """ String representation of a forum. """
-        return self.title
+        """String representation of a forum."""
+        return str(self.title)
 
     def get_questions(self):
-        """ Returns all questions associated with this forum. """
-        return self.questions.all()
+        """Returns all questions associated with this forum."""
+        return self.questions.all()  # pylint: disable=E1101
 
 
 class Post(models.Model):
-    """ Abstract model for a post. """
+    """Abstract model for a post."""
     description = models.TextField(verbose_name='')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-                               verbose_name='Author', default=1) # Substitua '1' pelo usuário padrão
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        verbose_name='Author', default=1  # Substitua '1' pelo usuário padrão
+    )
     created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name='Creation Date and Time')
+        auto_now_add=True, verbose_name='Creation Date and Time'
+    )
 
-    class Meta:
-        """ Meta class for Post. """
+    class Meta: # pylint: disable=R0903
+        """Meta class for Post."""
         abstract = True
 
     def __str__(self):
-        """ String representation of a post. """
-        return self.description[:50] + '...'
+        """String representation of a post."""
+        return str(self.description)[:50] + '...'
 
 
 class Question(Post):
-    """ Model for a question. """
+    """Model for a question."""
     title = models.CharField(max_length=100, verbose_name='')
-    forum = models.ForeignKey(Forum, on_delete=models.CASCADE,
-                              related_name='questions', verbose_name='Forum', default=3)
+    forum = models.ForeignKey(
+        Forum, on_delete=models.CASCADE,
+        related_name='questions', verbose_name='Forum', default=3
+    )
     favoritados = models.IntegerField(
-        default=0, verbose_name='Favorited Count')
+        default=0, verbose_name='Favorited Count'
+    )
     is_anonymous = models.BooleanField(default=False, verbose_name='')
     image = models.ImageField(
-        upload_to='media/question_images/', blank=True, null=True)
+        upload_to='media/question_images/', blank=True, null=True
+    )
     upvoters = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name='upvoted_questions', blank=True)
+        settings.AUTH_USER_MODEL, related_name='upvoted_questions', blank=True
+    )
 
     def __str__(self):
-        """ String representation of a question. """
-        return self.title
+        """String representation of a question."""
+        return str(self.title)
 
     def save(self, *args, **kwargs):
-        """ Save method for a question. """
+        """Save method for a question."""
         super().save(*args, **kwargs)
 
         if self.image:
-            img = Image.open(self.image.path)
+            img = Image.open(self.image.path)  # pylint: disable=E1101
             if img.height > 800 or img.width > 800:
                 output_size = (800, 800)
                 img.thumbnail(output_size)
-                img.save(self.image.path)
+                img.save(self.image.path)  # pylint: disable=E1101
 
     def toggle_upvote(self, user):
-        """ Toggles the upvote of a question for a user. """
-        if user in self.upvoters.all():
-            self.upvoters.remove(user)
+        """Toggles the upvote of a question for a user."""
+        if user in self.upvoters.all():  # pylint: disable=E1101
+            self.upvoters.remove(user)  # pylint: disable=E1101
         else:
-            self.upvoters.add(user)
+            self.upvoters.add(user)  # pylint: disable=E1101
         self.save()
 
     @property
     def upvote_count(self):
-        """ Returns the number of upvotes for a question. """
-        return self.upvoters.count()
+        """Returns the number of upvotes for a question."""
+        return self.upvoters.count()  # pylint: disable=E1101
 
 
 class Answer(Post):
-    """ Model for an answer. """
-    question = models.ForeignKey(Question, on_delete=models.CASCADE,
-                                 related_name='answers', verbose_name='Question', default=1)
+    """Model for an answer."""
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE,
+        related_name='answers', verbose_name='Question', default=1
+    )
     text = models.TextField(verbose_name='Answer Text')
     is_anonymous = models.BooleanField(
-        default=False, verbose_name='Modo anônimo')
+        default=False, verbose_name='Modo anônimo'
+    )
     image = models.ImageField(
-        upload_to='media/answer_images/', blank=True, null=True)
+        upload_to='media/answer_images/', blank=True, null=True
+    )
     upvoters = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name='upvoted_answers', blank=True)
+        settings.AUTH_USER_MODEL, related_name='upvoted_answers', blank=True
+    )
 
-    class Meta:
-        """ Meta class for Answer. """
+    class Meta: # pylint: disable=R0903
+        """Meta class for Answer."""
         verbose_name_plural = 'answers'
 
     def __str__(self):
-        """ String representation of an answer. """
-        return self.text[:50] + '...'
+        """String representation of an answer."""
+        return str(self.text)[:50] + '...'
 
     def save(self, *args, **kwargs):
-        """ Save method for an answer. """
+        """Save method for an answer."""
         super().save(*args, **kwargs)
 
         if self.image:
-            img = Image.open(self.image.path)
+            img = Image.open(self.image.path)  # pylint: disable=E1101
             if img.height > 800 or img.width > 800:
                 output_size = (800, 800)
                 img.thumbnail(output_size)
-                img.save(self.image.path)
+                img.save(self.image.path)  # pylint: disable=E1101
 
     def toggle_upvote(self, user):
-        """ Toggles the upvote of an answer for a user. """
-        if user in self.upvoters.all():
-            self.upvoters.remove(user)
+        """Toggles the upvote of an answer for a user."""
+        if user in self.upvoters.all():  # pylint: disable=E1101
+            self.upvoters.remove(user)  # pylint: disable=E1101
         else:
-            self.upvoters.add(user)
+            self.upvoters.add(user)  # pylint: disable=E1101
         self.save()
 
     @property
     def upvote_count(self):
-        """ Returns the number of upvotes for an answer. """
-        return self.upvoters.count()
+        """Returns the number of upvotes for an answer."""
+        return self.upvoters.count()  # pylint: disable=E1101
 
 
 class Notification(models.Model):
-    """ Model for a notification. """
+    """Model for a notification."""
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """ String representation of a notification. """
-        return f'Notification for {self.user.username} about question {self.question.title}'
+        """String representation of a notification."""
+        return (
+            f'Notification for {self.user.username} ' # pylint: disable=E1101
+            f'about question {self.question.title}' # pylint: disable=E1101
+        )
 
 
 class Report(models.Model):
-    """ Model for a report. """
+    """Model for a report."""
     REASON_CHOICES = [
         ('ofensivo', 'Conteúdo ofensivo'),
         ('irrelevante', 'Irrelevante para o fórum'),
@@ -148,9 +162,13 @@ class Report(models.Model):
     ]
 
     question = models.ForeignKey(
-        'main.Question', on_delete=models.CASCADE, related_name='reports', null=True, blank=True)
+        'main.Question', on_delete=models.CASCADE, related_name='reports',
+        null=True, blank=True
+    )
     answer = models.ForeignKey(
-        'main.Answer', on_delete=models.CASCADE, related_name='reports', null=True, blank=True)
+        'main.Answer', on_delete=models.CASCADE, related_name='reports',
+        null=True, blank=True
+    )
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     reason = models.CharField(max_length=20, choices=REASON_CHOICES)
@@ -158,5 +176,8 @@ class Report(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """ String representation of a report. """
-        return f"{self.get_reason_display()} - {self.user.username} - {'Question' if self.question else 'Answer'}"
+        """String representation of a report."""
+        return (
+            f"{self.get_reason_display()} - {self.user.username} - " # pylint: disable=E1101
+            f"{'Question' if self.question else 'Answer'}" # pylint: disable=E1101
+        )
