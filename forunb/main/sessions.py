cfa42c7624900c50a60bebd28e7d09f491a5a408
Davi-KLevy <daviklevy@gmail.com>
2024-08-15 10:33:38 -0300

    Pylint: refactoring sessions.py

--- forunb/main/sessions.py
+++ forunb/main/sessions.py
@@ -3,7 +3,7 @@ import requests
 
 URL = "https://sigaa.unb.br/sigaa/public/turmas/listar.jsf"
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36", # pylint: disable=C0301
     "Content-Type": "application/x-www-form-urlencoded",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.5",
