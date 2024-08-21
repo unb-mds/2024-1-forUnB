eb16659ae334d6854fb9672639389a22dc09f1bc
BrunoBReis <brunobragancadosreis@gmail.com>
2024-08-21 16:21:56 -0300

    bug: removing code from __inti__

--- forunb/forunb/settings/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from .base import *
-
-environment = os.getenv('DJANGO_ENVIRONMENT', 'local')
-
-if environment == 'production':
-    from .production import *
-else:
-    from .local import *
