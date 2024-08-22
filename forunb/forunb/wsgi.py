63edcf326968f07ad83db80081e0ca39514b9b45
BrunoBReis <brunobragancadosreis@gmail.com>
2024-08-22 09:40:22 -0300

    heroku: updating environ variabels

--- forunb/forunb/wsgi.py
+++ forunb/forunb/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 env.read_env(os.path.join(BASE_DIR, '.env'))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', env('DJANGO_SETTINGS_MODULE'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE'))
 
 application = get_wsgi_application()
