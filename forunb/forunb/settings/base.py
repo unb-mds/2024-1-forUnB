bfe2417862b32cc43b3b0060f15e654bfacab2e5
BrunoBReis <brunobragancadosreis@gmail.com>
2024-08-22 12:35:14 -0300

    heroku: updating static roots

--- forunb/forunb/settings/base.py
+++ forunb/forunb/settings/base.py
@@ -117,9 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
