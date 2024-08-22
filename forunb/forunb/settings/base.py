d7429cdddb66e2b06a98284b05dab7e44286caf1
Victor Bernardes <vmousee10@gmail.com>
2024-08-22 16:35:30 -0300

    chore: migrating the database to PostgreSQL

--- forunb/forunb/settings/base.py
+++ forunb/forunb/settings/base.py
@@ -71,11 +71,14 @@ WSGI_APPLICATION = 'forunb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'forunb_db',
+        'USER': 'forunb',
+        'PASSWORD': 'balao123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
