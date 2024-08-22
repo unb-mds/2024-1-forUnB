0285476eb645c221586727394726316bea7fd36e
BrunoBReis <brunobragancadosreis@gmail.com>
2024-08-22 09:40:33 -0300

    heroku: updating secret key

--- forunb/forunb/settings/base.py
+++ forunb/forunb/settings/base.py
@@ -19,7 +19,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Application definition
 
