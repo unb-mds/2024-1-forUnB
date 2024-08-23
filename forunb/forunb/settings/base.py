0c5773d37736a4ef7a76f6d4ad43b141e3848e2a
Davi Klein Levy <daviklevy@gmail.com>
2024-08-23 02:41:19 -0300

    Update base.py

--- forunb/forunb/settings/base.py
+++ forunb/forunb/settings/base.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
 ]
 
 # Configuração do WhiteNoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
