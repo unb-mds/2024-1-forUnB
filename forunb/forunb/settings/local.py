23764e49e09ba0ed8a4c8642f5083abc1fc29b16
BrunoBReis <brunobragancadosreis@gmail.com>
2024-08-23 09:09:04 -0300

    refactor: updating local

--- forunb/forunb/settings/local.py
+++ forunb/forunb/settings/local.py
@@ -4,7 +4,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', 'forunb-201a551c5a00.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
 
 DATABASES = {
     'default': {
