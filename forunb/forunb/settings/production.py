c932f1732adf7d0985dfc2715df843fa7478731d
Victor Bernardes <vmousee10@gmail.com>
2024-08-23 00:34:03 -0300

    feat: adding cloudinary in production code

--- forunb/forunb/settings/production.py
+++ forunb/forunb/settings/production.py
@@ -1,4 +1,8 @@
 """ Settings for production development. """
+import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 from .base import *
 import dj_database_url
 
@@ -13,3 +17,11 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
+
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.getenv('CLOUDINARY_URL'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+MEDIA_URL = 'https://res.cloudinary.com/%7Bcloud_name%7D/image/upload/'
