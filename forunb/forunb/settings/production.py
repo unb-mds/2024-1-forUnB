de5beade080f729e3084ebcf4d2f7df86b541051
Davi-KLevy <daviklevy@gmail.com>
2024-08-26 16:24:52 -0300

    host: configuring email_backend for production env

--- forunb/forunb/settings/production.py
+++ forunb/forunb/settings/production.py
@@ -27,3 +27,11 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = 'https://res.cloudinary.com/dmezdx5mc/image/upload/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'  # Isso é literal, use 'apikey' como o usuário
+EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY')  # A chave de API gerada no Heroku
+DEFAULT_FROM_EMAIL = 'noreply@forunb.com'
